cabfde853aba7cbdbade643965c2c01a376ded6d
NikolayLev <niko.original2@gmail.com>
2019-09-29 11:27:38 +0300

    start new educational project

---
+++ src/main/java/ru/levchenko/service/app/Application.java
@@ -0,0 +1,4 @@
+package ru.levchenko.service.app;
+
+public class Application {
+}
---
+++ src/main/java/ru/levchenko/service/models/User.java
@@ -0,0 +1,34 @@
+package ru.levchenko.service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String hashPassword;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Product> productList;
+
+
+}
